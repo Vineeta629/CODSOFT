2d8b77073dfc3caa0979807906cf248321f83471
Vineeta629 <vineetat648@gmail.com>
2024-06-27 13:36:04 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,28 +1,20 @@
-# task 1 CODSOFT
-print ("1 addition")
-print ("2 subraction")
-print("3 multiplication")
-print("4 division")
-
-choice = input("Enter your choice: ")
-                   
-num1= float(input("Enter Number1: "))
-num2= float(input("Enter Number2: "))
-
+print("1addition")
+print("2subraction")
+print("3multiplication")
+print("4division")
+choice =input("enter your choice:")
+num1= float(input("enter num 1:"))
+num2= float(input("enter num 2:"))
 if choice =="1":
-     print(num1,"+",num2,"=",(num1+num2))
+    print(num1,"+",num2,"=",(num1+num2))
 elif choice =="2":
-     print(num1,"-",num2,"=",(num1-num2))
-elif choice =="3":
-     print(num1,"*",num2,"=",(num1*num2))
-elif choice =="4":
-    if num2 == 0.0:
-        print("Divide by 0 Error ")
-    else:
+    print(num1,"-",num2,"=",(num1-num2))
+elif choice=="3":
+    print(num1,"*",num2,"=",(num1*num2))
+elif choice=="4":
+     if num2 == 0.0:
+        print ("divid eby 0 error")
+     else:
         print(num1,"/",num2,"=",(num1/num2))
 else:
     print("invalic choice")
-                                       
-                           
-                           
-                           
